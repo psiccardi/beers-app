6132c6a2b83fc06f9300d408f0218a9064e3ff6a
Pietro Maria Siccardi <pietro.siccardi@badgebox.com>
2024-02-15 23:02:54 +0100

    added logout button for mobile

--- resources/src/components/backoffice/Navbar.vue
+++ resources/src/components/backoffice/Navbar.vue
@@ -1,13 +1,24 @@
 <template>
     <b-navbar sticky="top" variant="light">
-            <div></div>
+            <div id="navbar-logout-btn" :onclick="logout">{{ t('logout') }}</div>
             <Locale />
     </b-navbar>
 </template>
 <script setup>
+    import { useI18n } from 'vue-i18n';
     import Locale from '../Locale.vue';
-
+    const {t, locale} = useI18n
+    const logout = function () {
+        window.location.href = APP_URL + '/logout';
+    }
 </script>
 <style scoped>
-
+    #navbar-logout-btn {
+        cursor: pointer;
+    }
+    @media (max-width: 762px) {
+        #navbar-logout-btn {
+            display: none;
+        }
+    }
 </style>
