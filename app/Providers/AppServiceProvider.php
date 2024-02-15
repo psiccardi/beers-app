f13ad6585896d282e75cc383cac9839d7d192341
beers-appme <beers-appme@tb-be04-linweb100.srv.teamblue-ops.net>
2024-02-15 22:19:42 +0100

    fix app/Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+	    //
+	    self::setCorrectAppUrl();
+	    \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+    }
+
+    public static function setCorrectAppUrl()
+    {
+        // ConfigurationManager::loadCorrectAppUrl();
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $host     = $_SERVER['HTTP_HOST'];
+            $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443 ? "https://" : "http://";
+            config(['app.url' => $protocol . $host]);
+        }
+        \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
     }
 }
