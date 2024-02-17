6dc3e387d652ba8e7c10b26852623d3297c5d40e
Pietro Maria Siccardi <pietro.siccardi@badgebox.com>
2024-02-17 19:08:41 +0100

    added comments

--- app/Classes/PunkApi.php
+++ app/Classes/PunkApi.php
@@ -6,14 +6,33 @@ use Illuminate\Support\Facades\Http;
 
 class PunkApi
 {
+    /**
+     * Punk API base url
+     */
     const BASE_URL='https://api.punkapi.com/';
+    /**
+     * Punk API version
+     */
     const VERSION='v2';
 
+    /**
+     * Method that retrieves API base url
+     *
+     * @return string
+     */
     protected static function getBaseUrl(): string
     {
         return self::BASE_URL . self::VERSION;
     }
 
+    /**
+     * Get beers API
+     *
+     * @param array the associative array of the request.
+     *              Should contain the keys 'page' and 'per_page' to work.
+     *
+     * @return array|null
+     */
     public static function getBeers(array $params = []): array|null
     {
         try {
