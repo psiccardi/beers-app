d35d81a9e888fc41ed63ed67b8371061c0e3fd29
Pietro Maria Siccardi <pietro.siccardi@badgebox.com>
2024-02-17 23:12:00 +0100

    added comments on api.js

--- resources/js/utilities/api.js
+++ resources/js/utilities/api.js
@@ -1,24 +1,51 @@
 import { getCookie } from "./cookies";
 
+/**
+ * Performs a PUT Api request with Content-Type: application/json.
+ * Automatically appends the X-XSRF-TOKEN header based on the value
+ * of the XSRF-TOKEN cookie, and adds Bearer Authorization header
+ * with the token retrieved from the cookie (if present)
+ *
+ * @param {String} url: the url of the request
+ *
+ * @param {Object} data: the parameters of the request
+ *
+ * @param {Function} success: callback called after a successful request
+ *
+ * @param {Function} error: callback called after an error
+ *
+ * @param {Number} signal: the signal of the request (used to cancel the request)
+ */
 export function jsonPutAPI(url, data, success, error, signal) {
     data = data || {};
     data.lang = localStorage.getItem('locale')
+    /**
+     * Getting the XSRF-TOKEN cookie decoded
+     */
     const xsrfToken = decodeURIComponent(getCookie('XSRF-TOKEN'));
+    /**
+     * Adding application/json value both to Content-Type and Accept headers
+     * Adding X-XSRF-TOKEN header
+     */
     const headers = {
         "Content-Type": "application/json; charset=utf-8",
         "Accept": "application/json",
         "X-XSRF-TOKEN": xsrfToken,
     }
+    /**
+     * Getting auth_token from cookie (this is the token retrieved after a successful login)
+     */
     const auth_token = getCookie('auth_token');
     if (auth_token) {
         headers.Authorization = `Bearer ${auth_token}`
     }
-    //data.user_token = BBAppSDK.USER_TOKEN;
+
+
     fetch(APP_URL + url, {
         method: "PUT",
         signal: signal,
         headers,
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), // Send data as an object
     })
         .then((response) => Promise.all([response, response.json()]))
         .then(([response, json]) => {
@@ -31,24 +58,49 @@ export function jsonPutAPI(url, data, success, error, signal) {
         .catch(error);
 }
 
+/**
+ * Performs a DELETE Api request with Content-Type: application/json.
+ * Automatically appends the X-XSRF-TOKEN header based on the value
+ * of the XSRF-TOKEN cookie, and adds Bearer Authorization header
+ * with the token retrieved from the cookie (if present)
+ *
+ * @param {String} url: the url of the request
+ *
+ * @param {Object} data: the parameters of the request
+ *
+ * @param {Function} success: callback called after a successful request
+ *
+ * @param {Function} error: callback called after an error
+ *
+ * @param {Number} signal: the signal of the request (used to cancel the request)
+ */
 export function jsonDeleteAPI(url, data, success, error, signal) {
     data = data || {};
     data.lang = localStorage.getItem('locale')
+    /**
+     * Getting the XSRF-TOKEN cookie decoded
+     */
     const xsrfToken = decodeURIComponent(getCookie('XSRF-TOKEN'));
+    /**
+     * Adding application/json value to Content-Type
+     * Adding X-XSRF-TOKEN header
+     */
     const headers = {
         "Content-Type": "application/json; charset=utf-8",
         "X-XSRF-TOKEN": xsrfToken,
     }
+    /**
+     * Getting auth_token from cookie (this is the token retrieved after a successful login)
+     */
     const auth_token = getCookie('auth_token');
     if (auth_token) {
         headers.Authorization = `Bearer ${auth_token}`
     }
-    //data.user_token = BBAppSDK.USER_TOKEN;
     fetch(APP_URL + url, {
         method: "DELETE",
         signal: signal,
         headers,
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), // Send data as an object
     })
         .then((response) => Promise.all([response, response.json()]))
         .then(([response, json]) => {
@@ -61,24 +113,49 @@ export function jsonDeleteAPI(url, data, success, error, signal) {
         .catch(error);
 }
 
+/**
+ * Performs a POST Api request with Content-Type: application/json.
+ * Automatically appends the X-XSRF-TOKEN header based on the value
+ * of the XSRF-TOKEN cookie, and adds Bearer Authorization header
+ * with the token retrieved from the cookie (if present)
+ *
+ * @param {String} url: the url of the request
+ *
+ * @param {Object} data: the parameters of the request
+ *
+ * @param {Function} success: callback called after a successful request
+ *
+ * @param {Function} error: callback called after an error
+ *
+ * @param {Number} signal: the signal of the request (used to cancel the request)
+ */
 export function jsonPostAPI(url, data, success, error, signal) {
     data = data || {};
     data.lang = localStorage.getItem('locale')
+    /**
+     * Getting the XSRF-TOKEN cookie decoded
+     */
     const xsrfToken = decodeURIComponent(getCookie('XSRF-TOKEN'));
+    /**
+     * Adding application/json value to Content-Type
+     * Adding X-XSRF-TOKEN header
+     */
     const headers = {
         "Content-Type": "application/json; charset=utf-8",
         "X-XSRF-TOKEN": xsrfToken,
     }
+    /**
+     * Getting auth_token from cookie (this is the token retrieved after a successful login)
+     */
     const auth_token = getCookie('auth_token');
     if (auth_token) {
         headers.Authorization = `Bearer ${auth_token}`
     }
-    //data.user_token = BBAppSDK.USER_TOKEN;
     fetch(APP_URL + url, {
         method: "POST",
         signal: signal,
         headers,
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), // Sending data as JSON object
     })
         .then((response) => Promise.all([response, response.json()]))
         .then(([response, json]) => {
@@ -91,6 +168,26 @@ export function jsonPostAPI(url, data, success, error, signal) {
         .catch(error);
 }
 
+/**
+ * Performs a GET Api request with Content-Type: application/json.
+ * Automatically appends the X-XSRF-TOKEN header based on the value
+ * of the XSRF-TOKEN cookie, and adds Bearer Authorization header
+ * with the token retrieved from the cookie (if present)
+ *
+ * NOTE: the main difference between this function and jsonGetAPI is
+ * that in this function we retrieve the response as text and in the other
+ * the response is JSON-decoded
+ *
+ * @param {String} url: the url of the request
+ *
+ * @param {Object} data: the parameters of the request
+ *
+ * @param {Function} success: callback called after a successful request
+ *
+ * @param {Function} error: callback called after an error
+ *
+ * @param {Number} signal: the signal of the request (used to cancel the request)
+ */
 export function getAPI(url, data, success, error, signal) {
     data = data || {};
     data.lang = localStorage.getItem('locale')
@@ -121,6 +218,22 @@ export function getAPI(url, data, success, error, signal) {
         .catch(error);
 }
 
+/**
+ * Performs a GET Api request with Content-Type: application/json.
+ * Automatically appends the X-XSRF-TOKEN header based on the value
+ * of the XSRF-TOKEN cookie, and adds Bearer Authorization header
+ * with the token retrieved from the cookie (if present)
+ *
+ * @param {String} url: the url of the request
+ *
+ * @param {Object} data: the parameters of the request
+ *
+ * @param {Function} success: callback called after a successful request
+ *
+ * @param {Function} error: callback called after an error
+ *
+ * @param {Number} signal: the signal of the request (used to cancel the request)
+ */
 export function jsonGetAPI(url, data, success, error, signal) {
     data = data || {};
     data.lang = localStorage.getItem('locale')
@@ -134,6 +247,14 @@ export function jsonGetAPI(url, data, success, error, signal) {
     if (auth_token) {
         headers.Authorization = `Bearer ${auth_token}`
     }
+    /**
+     * Url generation. This step transforms the Object 'data' into a query string
+     * then the url is generated by the concatenation of 'APP_URL' constant + 'url' variable and the generated
+     * query string.
+     * Example:
+     * var x = getUrlAPI('/', { key1: 1, key2: 2})
+     * console.log(x) --> prints 'http://localhost:8000?key1=1&key2=2'
+     */
     url = getUrlAPI(url, data);
     fetch(url, {
         method: "GET",
@@ -151,6 +272,20 @@ export function jsonGetAPI(url, data, success, error, signal) {
         .catch(error);
 }
 
+/**
+ * Returns a concatenation of 'APP_URL' constant with the variable 'path'
+ * and a query string generated from an object
+ * Example:
+ * const APP_URL = 'http://localhost:8000';
+ * getUrlAPI('/api/beers', {page: 0, limit: 10}) returns:
+ * http://localhost:8000/api/beers?page=0&limit=10
+ *
+ * @param {String} path
+ *
+ * @param {String} params
+ *
+ * @returns
+ */
 export function getUrlAPI(path, params) {
     let url = APP_URL + path;
 
@@ -160,6 +295,16 @@ export function getUrlAPI(path, params) {
     return url;
 }
 
+/**
+ * Recursive function that generates a query string
+ * from an object.
+ *
+ * @param {Object} obj
+ *
+ * @param {String} prefix
+ *
+ * @returns
+ */
 export function serializeParams(obj, prefix) {
     var str = [],
         p;
@@ -185,22 +330,80 @@ export function serializeParams(obj, prefix) {
     return str.join("&");
 }
 
+/**
+ * Web app login
+ * After a successful response the user is authenticated with two guards:
+ * - sanctum
+ * - default guard based on database sessions
+ *
+ * @param {Object} data
+ *
+ * @param {Function} success
+ *
+ * @param {Function} error
+ *
+ * @param {Number} signal
+ */
 export function loginWebAPI(data, success, error, signal) {
     jsonPostAPI('/login', data, success, error, signal)
 }
 
+/**
+ * Web app logout (unused)
+ *
+ * @param {Object} data
+ *
+ * @param {Function} success
+ *
+ * @param {Function} error
+ *
+ * @param {Number} signal
+ */
 export function logoutWebAPI(data, success, error, signal) {
     jsonGetAPI('/logout', data, success, error, signal)
 }
 
+/**
+ * Login using sanctum guard only. No cookies/sessions
+ *
+ * @param {Object} data
+ *
+ * @param {Function} success
+ *
+ * @param {Function} error
+ *
+ * @param {Number} signal
+ */
 export function loginAPI(data, success, error, signal) {
     jsonPostAPI('/api/login', data, success, error, signal);
 }
 
+/**
+ * Gets current user
+ *
+ * @param {Object} data
+ *
+ * @param {Function} success
+ *
+ * @param {Function} error
+ *
+ * @param {Number} signal
+ */
 export function getUserAPI(data, success, error, signal) {
     jsonGetAPI('/api/user', data, success, error, signal);
 }
 
+/**
+ * Gets beers list
+ *
+ * @param {Object} data
+ *
+ * @param {Function} success
+ *
+ * @param {Function} error
+ *
+ * @param {Number} signal
+ */
 export function getBeersAPI(data, success, error, signal) {
     jsonGetAPI('/api/beers', data, success, error, signal);
 }
